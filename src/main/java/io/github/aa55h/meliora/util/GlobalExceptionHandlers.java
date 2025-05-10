64590e8a9be5ed8a3bc07dff57da1f1942e39e84
KoblizekXD <janprokupek@centrum.cz>
2025-05-10 21:02:38 +0200

    Global exception handlers

---
+++ src/main/java/io/github/aa55h/meliora/util/GlobalExceptionHandlers.java
@@ -0,0 +1,32 @@
+package io.github.aa55h.meliora.util;
+
+import io.github.aa55h.meliora.dto.GenericErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandlers {
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<GenericErrorResponse> handleAuthenticationException(HttpServletRequest request, AuthenticationException e) {
+        GenericErrorResponse errorResponse = new GenericErrorResponse(
+                e.getMessage(),
+                request.getContextPath(),
+                401,
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.status(401).body(errorResponse);
+    }
+    
+    @ExceptionHandler(FileServiceException.class)
+    public ResponseEntity<GenericErrorResponse> handleFileServiceException(HttpServletRequest request, FileServiceException e) {
+        GenericErrorResponse errorResponse = new GenericErrorResponse(
+                e.getMessage(),
+                request.getContextPath(),
+                500,
+                System.currentTimeMillis()
+        );
+        return ResponseEntity.status(500).body(errorResponse);
+    }
+}
