744d4b0151c474d956c9cf350e66a718f83635b7
KoblizekXD <janprokupek@centrum.cz>
2025-05-27 20:02:14 +0200

    Generating client code for backend

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,20 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+import java.nio.file.Files
+import java.nio.file.StandardCopyOption
+
+buildscript {
+    configurations.all {
+        resolutionStrategy {
+            force("org.yaml:snakeyaml:1.33")
+        }
+    }
+}
+
 plugins {
     java
     id("org.springframework.boot") version "3.5.0-RC1"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "6.6.0"
 }
 
 group = "io.github.aa55h"
@@ -56,6 +69,36 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
+val specDirectory = layout.buildDirectory.dir("generated/openapi").get().asFile
+val specFile = specDirectory.resolve("spec.yaml")
+
+tasks.register("prepareOpenApiSpec") {
+    group = "documentation"
+    doLast {
+        Files.createDirectories(specDirectory.toPath())
+    }
+}
+
+tasks.register("generateOpenApiSpec") {
+    group = "documentation"
+    description = "Generates OpenAPI specification from the application"
+    dependsOn("prepareOpenApiSpec")
+    doLast {
+        uri("http://localhost:8080/v3/api-docs.yaml").toURL().openStream().use {
+            Files.copy(it, specFile.toPath(), StandardCopyOption.REPLACE_EXISTING)
+        }
+    }
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.register<GenerateTask>("generateDartClient") {
+    group = "openapi tools"
+    description = "Generate Dart client from OpenAPI specification"
+    dependsOn("generateOpenApiSpec")
+    inputSpec = specFile.absolutePath
+    outputDir = layout.buildDirectory.dir("meliora-client-api").get().asFile.absolutePath
+    generatorName = "dart"
 }
